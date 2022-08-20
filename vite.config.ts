23fd3826c211955e7079fdc84399c206e058ebc8
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-08-20 11:49:25 +0800

    0.0.0

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
+  plugins: [
+    dts({ insertTypesEntry: true })
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
