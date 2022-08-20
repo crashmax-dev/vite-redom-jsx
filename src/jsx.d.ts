ce0eb134b164634eea3efee1aa3fdf06cd72fed2
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-08-20 13:36:30 +0800

    upd

--- src/jsx.d.ts
+++ src/jsx.d.ts
@@ -1,6 +1,8 @@
+import type { RedomElement } from 'redom'
+
 export declare global {
   export declare namespace JSX {
-    export interface Element { }
+    export interface Element extends RedomElement { }
     export interface HTMLElement { }
     export interface IntrinsicElements {
       [tag: string]: BaseProps
