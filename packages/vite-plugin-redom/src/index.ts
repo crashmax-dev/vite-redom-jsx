28bab7990c1b0b47b000c63729a03185ae27f393
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-08-21 13:31:15 +0800

    release 1.0.0

--- packages/vite-plugin-redom/src/index.ts
+++ packages/vite-plugin-redom/src/index.ts
@@ -1,4 +1,19 @@
-import { redom } from './plugin.js'
+import type { PluginOption } from 'vite'
+
+function redom(): PluginOption {
+  return {
+    name: 'vite-plugin-redom',
+    config() {
+      return {
+        esbuild: {
+          jsxInject: `import { h, Fragment } from 'redom-jsx'`,
+          jsxFactory: 'h',
+          jsxFragment: 'Fragment'
+        }
+      }
+    }
+  }
+}
 
 export { redom }
 export default redom
