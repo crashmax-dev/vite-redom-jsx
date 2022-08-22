1d6376fbca4540908e9c3a2516247c0967d0e2f9
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-08-22 14:02:59 +0800

    chore: update tsup config

--- packages/jsx/tsup.config.ts
+++ packages/jsx/tsup.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig({
+export default defineConfig((config) => ({
   entry: ['src/index.ts'],
   format: ['esm', 'cjs'],
   clean: true,
   minify: true,
-  dts: true
-})
+  dts: true,
+  watch: config.watch
+}))
