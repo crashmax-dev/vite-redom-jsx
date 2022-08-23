6ad7f751bf5ebf4c4ca9026dfbae7ee3b8b33f97
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-08-23 13:06:15 +0800

    `redom-jsx` v1.1.1

--- packages/redom-jsx/src/index.ts
+++ packages/redom-jsx/src/index.ts
@@ -2,10 +2,21 @@ import { el, mount, unmount, setAttr, setChildren } from 'redom'
 import type { RedomComponent } from 'redom'
 import './types.d.ts'
 
-export const Fragment = (_: unknown, ...childrens: RedomComponent[]) => {
-  const fragment = document.createDocumentFragment()
-  setChildren(fragment, childrens)
-  return fragment
+class Fragment {
+  public el: DocumentFragment
+
+  constructor(_: unknown, ...children: RedomComponent[]) {
+    const fragment = document.createDocumentFragment()
+    setChildren(fragment, children)
+    this.el = fragment
+  }
 }
 
-export { el, mount, unmount, setAttr, setChildren }
+export {
+  el,
+  Fragment,
+  mount,
+  unmount,
+  setAttr,
+  setChildren
+}
